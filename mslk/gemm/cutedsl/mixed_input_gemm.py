29fa62dd556d9c9a51606d32e61c471f9662482e
Josh Fromm <jwfromm@meta.com>
2026-03-01 11:58:50 -0800

    CuteDSL MXFP8 GEMM (#180)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/MSLK/pull/180
    
    This diff adds a cutedsl implementation for standard MXFP8 GEMM along with an optimized quantization routine that is quite a bit faster than our previous torch based approach. While the GEMM itself isnt faster than `torch._scaled_mm`, which is backed by cublasLt, it is a useful example and can be adjusted for various other functionality.
    
    Some benchmarking results to get a sense of performance:
    ```
    -----------------------------------------------------------------------------------------------------------------------------------
    OpName                          (M, N, K)                 Sim        Ms         TFLOPS     GB/s       Mem BW Util %  Compute Util %
    -----------------------------------------------------------------------------------------------------------------------------------
    TorchFP8Rowwise                 (8192, 8192, 8192)        11.500     0.664      1655.09    404.08     5.10           33.10
    TorchMXFP8Groupwise             (8192, 8192, 8192)        11.688     0.598      1839.28    449.04     5.66           36.79
    CuteDSLMXFP8                    (8192, 8192, 8192)        11.688     0.659      1668.77    407.42     5.14           33.38
    
    TorchFP8Rowwise                 (2048, 8192, 5120)        7.188      0.127      1357.48    679.40     8.57           27.15
    TorchMXFP8Groupwise             (2048, 8192, 5120)        7.281      0.124      1389.14    695.25     8.77           27.78
    CuteDSLMXFP8                    (2048, 8192, 5120)        7.281      0.131      1311.68    656.48     8.28           26.23
    
    TorchFP8Rowwise                 (256, 8192, 5120)         7.188      0.036      604.65     1335.95    16.85          12.09
    TorchMXFP8Groupwise             (256, 8192, 5120)         7.281      0.039      557.14     1230.99    15.53          11.14
    CuteDSLMXFP8                    (256, 8192, 5120)         7.281      0.038      559.40     1235.99    15.59          11.19
    
    TorchFP8Rowwise                 (8, 8192, 5120)           7.250      0.034      19.99      1254.34    15.82          0.40
    TorchMXFP8Groupwise             (8, 8192, 5120)           7.406      0.039      17.08      1072.08    13.52          0.34
    CuteDSLMXFP8                    (8, 8192, 5120)           7.406      0.038      17.84      1119.66    14.12          0.36
    
    ```
    
    Reviewed By: jiawenliu64, cthi
    
    Differential Revision: D94170428

--- mslk/gemm/cutedsl/mixed_input_gemm.py
+++ mslk/gemm/cutedsl/mixed_input_gemm.py
@@ -1,33 +1,10 @@
 # @nolint # fbcode
-# Copyright (c) 2025 - 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# A direct copy from https://github.com/NVIDIA/cutlass/blob/a4eb0e05f6dd0403f94087b495393bdca75bf0ad/examples/python/CuTeDSL/blackwell/mixed_input_gemm.py
+# pyre-ignore-all-errors
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 
 import argparse
 from math import ceil, log2
