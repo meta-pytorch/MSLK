92e2774f189a222ed8346b7369567d27713b907c
Henry Tsang <henrylhtsang@meta.com>
2026-04-08 14:56:51 -0700

    Apply ufmt formatting and fix F821 lint error (#208)
    Summary:
    - Apply ufmt (usort + black) formatting to all Python files
    - Add missing is_dQ_wg parameter to mma_one_m_block in flash_bwd_sm90.py
      to fix F821 undefined name lint error
    - Excludes flash_attn/cute/ and mslk/fb/external/quack/ per lint config
    
    Reviewed By: cthi
    
    Differential Revision: D100042104
    
    fbshipit-source-id: 813cbbfb225cac5c522d506e37dfbad893f8a335

--- mslk/attention/flash_attn/interface.py
+++ mslk/attention/flash_attn/interface.py
@@ -8,21 +8,23 @@
 def not_implemented(*args, **kwargs):
     raise NotImplementedError("This function is not implemented.")
 
+
 try:
     from mslk.fb.mslk.attention.flash_attn import flash_attn_func as _flash_attn_func
     from mslk.fb.mslk.attention.flash_attn.interface import (
         flash_attn_combine,
         mslk_flash_attn_bwd as _flash_attn_bwd,
-        mslk_flash_attn_fwd as _flash_attn_fwd,
         mslk_flash_attn_decode as _flash_attn_decode,
+        mslk_flash_attn_fwd as _flash_attn_fwd,
     )
 except ImportError:
     from flash_attn.cute.interface import (
-        flash_attn_func as _flash_attn_func,
-        flash_attn_combine,
         _flash_attn_bwd,
         _flash_attn_fwd,
+        flash_attn_combine,
+        flash_attn_func as _flash_attn_func,
     )
+
     _flash_attn_decode = not_implemented
 
 flash_attn_func = _flash_attn_func
